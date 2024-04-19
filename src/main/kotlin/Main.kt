efc281f784ba26adafb7b66686121f52427f8359
Ko4Learner <122316248+Ko4Learner@users.noreply.github.com>
2024-04-19 16:30:41 +0300

    Job(19.04.2024) inline fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,50 @@
 import java.util.Scanner
 
-
-interface Name {
-    val name: String
+interface CreateNew {
+    fun create()
 }
 
-data class Archive(override val name: String) : Name {
-    val noteMap: MutableMap<Int, String> = mutableMapOf()
+data class Archive(val name: String) : {
+    val noteMap: MutableList<Note> = mutableListOf()
 }
 
-data class Note(override val name: String) : Name {
+data class Note(val name: String) {
     val content: String = ""
 }
 
-fun <T : Name> printMenu(map: MutableMap<Int, T>) {
-    println("Список архивов: \n0. Создать архив \n$map \n${map.size + 1}. Выход")
+inline fun <reified T> printMenu(list: MutableList<T>) {
+    println("Список архивов: \n0. Создать архив \n$list \n${list.size + 1}. Выход")
 }
 
-fun <T : Name> commandScanner(map: MutableMap<Int, T>) {
+inline fun <reified T> commandScanner(list: MutableList<T>) {
     while (true) {
         val command = Scanner(System.`in`).nextLine()
         when (command.toInt()) {
-            0 -> create(map)
-            map.size + 1 -> return
+            0 -> create(list)
+            list.size + 1 -> return
         }
     }
 }
 
-fun <T : Name> create(map: MutableMap<Int, T>) {
+inline fun <reified T> create(list: MutableList<T>) {
     println("Введите название:")
+
     val name: String = Scanner(System.`in`).nextLine()
-    // map[map.size + 1] =
-    println("$name создан")
+    when (list.firstOrNull()) {
+        is Archive -> list.add(Archive(name))
+        is Note -> {
+            println("Введите содержание заметки:")
+            val content =
+            list.add(Note(name, Scanner(System.`in`).nextLine()))
+        }
+    }
+
 }
 
 fun main() {
 
-    val archiveMap: MutableMap<Int, Archive> = mutableMapOf()
-    printMenu(archiveMap)
-    commandScanner(archiveMap)
+    val archiveList: MutableList<Archive> = mutableListOf()
+    printMenu(archiveList)
+    commandScanner(archiveList)
 
 }
